248d935e463d6973fce91b481501e1c74105f473
FedorPop <fedor.popovic30@gmail.com>
2022-03-16 20:07:52 +0100

    cypress

---
+++ cypress/integration/board.spec.js
@@ -0,0 +1,25 @@
+/// <reference types='cypress'/>
+import { newBoard } from "../pageObject/board";
+import { loginPage } from "../pageObject/login"
+import { myOrganizations } from "../pageObject/my-organizations";
+
+describe('board', ()=>{
+
+    before('login', ()=>{
+        cy.visit('/login')
+        loginPage.login('zxcv@zxcv.com', 'zxcvzxcvzxcv123');
+    })
+
+    it('chose organization', ()=>{
+        cy.url().should('contain', '/my-organizations');
+        cy.get('h2').should('be.visible');
+        cy.get('.organization-list-item').eq(0).click();
+        myOrganizations.modalConfirmBtn.click();        
+    })
+
+    it('add new board', ()=>{
+        
+        newBoard.createBoard('novi Board');
+        
+    })
+})
